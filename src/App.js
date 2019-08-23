23ee81c8abdb3bcff74c112c97c78dc2259c8d24
rai8 <hytonne@outlook.com>
2019-08-23 18:24:13 +0300

    styling in jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+export default class App extends Component {
+  //name = "Rhytonne";
+  render() {
+    const style = {
+      border: "solid",
+      textAlign: "center",
+      boxShadow: "2px 2px"
+    };
+    return (
+      <div>
+        <div style={style}>Style</div>
+        <button id="hover">Click me</button>
+      </div>
+    );
+  }
 }
-
-export default App;
+/* const AppINstance = new App(); //initalizing a new instance for app
+console.log(AppINstance.name); //this will work since name is a property
+ */
