a47f2a62b3fcc901457174838ac5b4130027c6f8
rai8 <hytonne@outlook.com>
2019-08-24 00:35:51 +0300

    using the previous state in setState call back method

--- src/components/Component2.js
+++ src/components/Component2.js
@@ -4,7 +4,8 @@ export default class Component2 extends Component {
   constructor() {
     super();
     this.state = {
-      propState: "This is a class component"
+      propState: "This is a class component",
+      propState2: 5
     };
   }
   /*  //this is the wrong way we're not supposed to mutate state directly
@@ -13,14 +14,26 @@ export default class Component2 extends Component {
   }; */
 
   //this is the correct syntax
-  changeState = () => {
-    this.setState({ propState: "This is a changed state using setState" });
-  };
+  changeState = () =>
+    this.setState((prevState, props) => ({
+      propState2: prevState.propState2 + 1
+    }));
+  changeState2 = () =>
+    this.setState({
+      propState: this.state.propState + "R"
+    });
+
   render() {
     return (
       <div>
         <button onClick={() => this.changeState()}>Click here</button>
+        <button onClick={() => this.changeState2()}>Click here</button>
+
+        <br />
+        <br />
+        {this.state.propState2}
         {this.state.propState}
+        <br />
       </div>
     );
   }
